1e1d607cfd62452ee5ea9b444643439da9116610
Gustavo Eidt <gustavo.eidt@gmail.com>
2020-11-04 14:06:20 -0300

    calculo de atraso no projeto

--- desafio-euax/app/Models/Projeto.php
+++ desafio-euax/app/Models/Projeto.php
@@ -22,4 +22,31 @@ class Projeto extends Model
     {
         return $this->hasMany('App\Models\Atividade');
     }
+
+    public function progresso()
+    {
+        // Calcula o andamento do projeto
+        // (atividades finalizadas / total de atividades ) * 100
+        // Arredondamento para baixo e conversão para int
+        $totalAtividades = $this->hasMany('App\Models\Atividade')->count();
+        $totalFinalizadas = $this->hasMany('App\Models\Atividade')->where('finalizada',1)->count();
+        
+        // Verifica se algum dos valores é zero
+        if ($totalAtividades*$totalFinalizadas) {
+            $progresso = (int)floor(($totalFinalizadas / $totalAtividades) * 100);
+        } else {
+            $progresso = 0;
+        }
+        return $progresso;
+    }
+
+    public function atrasado()
+    {
+        // Busca as atividades não finalizadas
+        $atividadesNaoFinalizadas = $this->hasMany('App\Models\Atividade')->where('finalizada',0);
+        // Verifica se há alguma com a data de fim maior que a data de fim do projeto
+        $emAtraso = $atividadesNaoFinalizadas->whereDate('fim','>',$this->fim->format('Y-m-d'))->count();
+
+        return ($emAtraso ? true : false);
+    }
 }
